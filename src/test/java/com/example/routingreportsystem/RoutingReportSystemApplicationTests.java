a7aa16a4d8ca6677e0c51e0952a25db349d1510f
alireza gholami <gholamialireza1004@gmail.com>
2023-09-15 13:52:33 +0330

    initial project

---
+++ src/test/java/com/example/routingreportsystem/RoutingReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routingreportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
